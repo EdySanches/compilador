14c1f1593bd7d23bc2491d5c69c2da9f36351b7a
unknown <ra177214@ACAD.UCDB.BR>
2023-03-27 18:38:34 -0400

    [NEW] asd

--- src/sintatico.py
+++ src/sintatico.py
@@ -1,9 +1,11 @@
 def get_sint_token(lex_tokens, sint_token):
     
-    
-    
-    
-    
-    
-    
+    program_fun(lex_tokens)
+       
     pass
+
+
+def program_fun(lex_token):
+    
+    if lex_token == "":
+        pass
